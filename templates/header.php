4303c853532a44922b3db217165f72ab3ba935c4
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2022-10-17 23:34:38 -0400

    Testing something

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 !<!DOCTYPE html>
 <html>
     <head>
-        <title>Singapire Management University Peer Evaluation </title>
+        <title>Singapore Management University Peer Evaluation </title>
     </head>
 </html>
