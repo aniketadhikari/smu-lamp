2ed0c7bf5053a67de824a1c351f91b1bfb675669
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2022-10-17 23:29:18 -0400

    10/17 11:29

---
+++ pages/anotherPage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My second PHP page</h1>
+
+<?php
+echo "Is this thing on?"; 
+?>
+ 
+</body>
+</html>
---
+++ pages/index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php require("./templates/header.php");?>
+<h1>My first PHP page</h1>
+</body>
+</html>
