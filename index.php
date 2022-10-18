c5b7bf5722580d11ed42bf643323678e3bfcf63a
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2022-10-18 13:19:34 -0400

    10/18 1:19

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- CSS File -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <!-- Start of header -->
+    <?php include 'components/user_header.php' ?>
+    <!-- End of header -->
+    
+</body>
+</html>
