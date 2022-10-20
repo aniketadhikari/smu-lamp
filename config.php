90a040f5275385e6ec646784601b55c4e668f101
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2022-10-19 21:39:44 -0400

    Connection

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
 // connect to localhost server, using username root and blank password to connect to 
 // database called login_sample_db
 
-$conn = mysqli_connect('localhost:8888','root','EMRslAL8Q8Mj','SMU');
+$conn = mysqli_connect('localhost','root','EMRslAL8Q8Mj','SMU', 8888);
 
 if (!$conn) {
     echo "Connection could not be established";
