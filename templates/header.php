66470f0132cc962ad7529c71c5bbb3ca738d0f3c
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2022-10-20 21:56:17 -0400

    Added a dropdown on home page

--- templates/header.php
+++ templates/header.php
@@ -1,16 +1,32 @@
 <html>
-    <head>
-        <!-- <link rel="stylesheet" href="../css/styles.css"> -->
-    </head>
-        <div id="top-header" style="background-color: #151c55;">
-            <div id="logo">
-                <img src="https://iafor.org/wp-content/uploads/2020/02/Singapore-Management-University-SMU-Logo.jpg" alt="SMU Logo" width="10%" height="10%">    
-            </div>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                </ul>
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<header>
+    <div class="t-header">
+        <div class="logo">
+            <img src="https://www.smu.edu.sg/themes/smubase_4g/svg/logo-d-smu.svg" alt="SMU Logo" width="100px" style="background-color: white;">
+            <img src="https://www.smu.edu.sg/themes/smubase_4g/svg/oblique.svg" alt="oblique" height="100px">
+        </div>
+        <div class="navbar">
+            <nav style="background-color:#151c55; display:flex">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <div class="dd">
+                    <button class="ddbtn">Dropdown</button>
+                    <div class="dd-content">
+                        <a href="https://cis.smu.edu.sg/">Bachelor of Integrative Studies</a>
+                        <a href="https://admissions.smu.edu.sg/programmes/school-accountancy/school-accountancy">School of Accountancy</a>
+                        <a href="https://admissions.smu.edu.sg/programmes/lee-kong-chian-school-business/bachelor-business-management">Bachelor of Business Administration</a>
+                    </div>
+                </div>
             </nav>
         </div>
-    
+        <div class="logo" style="transform: scaleX(-1);">
+            <img src="https://www.smu.edu.sg/themes/smubase_4g/svg/oblique.svg" alt="oblique" height="100px">
+        </div>
+    </div>
+</header>
+
 </html>
